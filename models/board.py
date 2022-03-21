574544e307bb3a404bfa2ae03868309edc30d6d5
Simon2833 <szymin.gruszecku@gmail.com>
2022-03-21 14:31:11 +0100

    From now I am going to write in english and I hope you will do the same. I added basic villager class and 3 other classes archer, spearman and warrior which inherits from villager class.

---
+++ models/board.py
@@ -0,0 +1,14 @@
+#board class---------
+class board:
+    def __init__(self, wspx, wspy):
+        self.wspx = wspx
+        self.wspy = wspy
+
+
+
+#board initialization-------
+'''print("How big the board should be?")
+x = int(input("Please input width of the board: "))
+y = int(input("Please input height of the board: "))
+map = board(x, y)
+print("x: " + str(map.wspx) + "\ny: "  + str(map.wspy))'''
