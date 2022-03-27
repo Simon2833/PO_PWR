d8b674137409d613c1744948c6fd35a3cea9ea17
Simon2833 <90138007+Simon2833@users.noreply.github.com>
2022-03-27 13:02:49 +0200

    I have changed function name from random_pos to randomPos bc I forgot it earlier

--- unit_tests/test_math.py
+++ unit_tests/test_math.py
@@ -8,6 +8,6 @@ class test_random_pos(unittest.TestCase):
     def test_return_correct_tab(self):
         width = random.randint(1, 1000)
         height = random.randint(1, 1000)
-        tab = math.random_pos(width, height)
+        tab = math.randomPos(width, height)
         assert (0 <= tab[0] < 1000)
         assert (0 <= tab[1] < 1000)
