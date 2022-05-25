6f56a4f9c711cbe70ff8c07e636f56b44386cebc
Simon2833 <szymin.gruszecku@gmail.com>
2022-05-25 15:08:32 +0200

    Addem moving function only for monsters

--- main.py
+++ main.py
@@ -1,41 +1,54 @@
 import models
 import time
+import sys
+import ui.MenuLayout
+from PyQt6.QtWidgets import *
 
-
-start_time = time.time()
-# Starting variables which user will be inputting
-maxMonster = 5
-maxFood = 5
-maxTribes = 5
 
 
 def main():
+    # MENU SETUP
+    app = QApplication(sys.argv)
+    Form = QWidget()
+    menu = ui.MenuLayout.Ui_Form()
+    menu.setupUi(Form)
+    Form.show()
+    app.exec()
+    start_time = time.time() # Moved here to substract menu time from program running time
+
+    # GRABBING USER INPUT VALUES
+    maxMonster = int(menu.MonsterAmnt_prev.text())
+    maxFood = int(menu.StartFood_prev.text())
+    maxTribes = int(menu.TribeAmount_prev.text())
+
     # BOARD INITIALIZING
-    board = models.board(10, 10)
+    board = models.board(int(menu.dimX_prev.text())*5, int(menu.dimY_prev.text())*5)
     tab = board.boardInit()
 
     # BOARD GENERATING
     tab = board.boardGenerate(tab, maxFood, maxMonster, maxTribes)
 
-    # BOARD PRINTING
-    for y in range(len(tab)):
-        print()
-        for x in range(len(tab[y])):
-            print(tab[y][x], end="|")
+    for i in range(0, 10):
+        # BOARD PRINTING
+        for y in range(len(tab)):
+            print()
+            for x in range(len(tab[y])):
+                print(tab[y][x], end="|")
 
-    print("--- %s seconds ---" % (time.time() - start_time))
-    # CHECKING BOARD IN RANGE OF EVERY OBJECT ON THE BOARD
-    for i in range(len(models.monster.monsterList)):
-        bruh = models.monster.monsterList[i]
-        xd = bruh.checkRange(tab, bruh.range, bruh.cox, bruh.coy, bruh.type, bruh.id, bruh.tribe, bruh.job)
-        print(xd)
-    for base in models.villageBase.baseList:
-        for unit in base.populationList:
-            xd = unit.checkRange(tab, unit.range, unit.cox, unit.coy, unit.type, unit.id, unit.tribe, unit.job)
-            print(xd)
+        print("--- %s seconds ---" % (time.time() - start_time))
+        # CHECKING BOARD IN RANGE OF EVERY OBJECT ON THE BOARD
+        # for bruh in models.monster.monsterList:
+        #     enemy, sighted = bruh.checkRange(tab, bruh)
+        #     print(enemy, sighted)
+        # for base in models.villageBase.baseList:
+        #     for bruh in base.populationList:
+        #         enemy, sighted = bruh.checkRange(tab, bruh)
+        #         print(enemy, sighted)
+        print("--- %s seconds ---" % (time.time() - start_time))
+        for monster in models.monster.monsterList:
+            monster.move(tab, monster)
 
 
 
 main()
 
-print("--- %s seconds ---" % (time.time() - start_time))
